396697a2a17318b2101d2004dac0964c6d1b3589
Jaison M <jaison.mora@mcets-inc.com>
2023-10-11 16:33:22 -0500

    version dos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -17,5 +16,10 @@ class Post extends Model
         return Attribute::make(
             get: fn (mixed $value, array $attributes) => substr($attributes['content'], 0, 120),
         );
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('v1/post', PostController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+Route::apiResource('v1/post', PostV1::class)->only(['index', 'show', 'destroy']);
+Route::apiResource('v2/post', PostV2::class)->only(['index', 'store', 'show', 'update', 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
