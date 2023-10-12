79c16acaa8a4131215687d2734f9a41a2bbfedfa
Jaison M <jaison.mora@mcets-inc.com>
2023-10-12 09:43:50 -0500

    autenticacion con sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::apiResource('v1/post', PostV1::class)->only(['index', 'show', 'destroy'])->middleware('auth:sanctum');
 Route::apiResource('v2/post', PostV2::class)->only(['index', 'store', 'show', 'update', 'destroy'])->middleware('auth:sanctum');
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('login', [LoginController::class, 'login']);
